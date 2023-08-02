80ec629100128cd10ca8039263c7241370325a5f
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2023-08-02 17:35:37 +0200

    Update Utilities.java

--- app/src/main/java/com/twinfishlabs/precamera/Utilities.java
+++ app/src/main/java/com/twinfishlabs/precamera/Utilities.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import com.umeng.analytics.MobclickAgent;
 
 import android.app.Activity;
 import android.app.AlertDialog;
